7c5dd172eca502471efb90ef3f798b81d3da3b2c
jessica <laurenciajessica16e@gmail.com>
2026-06-06 19:32:27 +0700

    contoh

---
+++ Praktikum12/src/praktikum12/DiskonLambda.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikum12;
+
+interface IDiskon {
+    public double hitungDiskon(int harga);
+}
+
+public class DiskonLambda {
+    public static void main(String[] args) {
+
+        // tanpa lambda
+        IDiskon diskonMerdeka = new IDiskon() {
+            public double hitungDiskon(int harga) {
+                return harga - (harga * 0.3);
+            }
+        };
+
+        // dengan lambda
+        IDiskon diskonLebaran = (harga) -> harga - (harga * 0.4);
+
+        // dengan lambda dengan blok statement
+        IDiskon diskonBiasa = (harga) -> {
+            return harga - (harga * 0.1);
+        };
+
+        System.out.println("Diskon Merdeka: " + diskonMerdeka.hitungDiskon(45000));
+        System.out.println("Diskon Lebaran: " + diskonLebaran.hitungDiskon(45000));
+        System.out.println("Diskon Biasa: "   + diskonBiasa.hitungDiskon(45000));
+    }
+}
